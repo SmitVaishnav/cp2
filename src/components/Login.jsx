77906f1d7567e2bba3555cf56ce6841806c838b6
darshan-padia <darshanpadia5@gmail.com>
2024-05-09 01:42:29 +0530

    fixed login.js

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,15 @@
-import { Input } from "postcss";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import bcrypt from "bcryptjs";
 
 function Login() {
+  const navigate =  useNavigate();
   const handleLogin = (e) => {
     e.preventDefault();
     const hashedPassword = bcrypt.hashSync(
       e.target.password.value,
       "$2a$10$CwTycUXWue0Thq9StjUM0u"
     );
-
     fetch("http://localhost:3000/api/users/login", {
       method: "POST",
       headers: {
@@ -24,15 +23,18 @@ function Login() {
       .then((response) => {
         document.cookie = `username=${e.target.email.value}; path=/`;
         document.cookie = `password=${hashedPassword}; path=/`;
+
+        navigate('/');
       })
       .catch((error) => {
         alert("Invalid email or password");
         console.log(error);
       });
   };
+
   return (
     <div className="flex h-screen bg-[url('./assets/64a6d9e3b3affa8513290f40_account-gradient.jpg')]">
-      <div className=" w-1/2 h-screen  flex flex-col  justify-center px-24 -mt-20 leading-[6.2vw]">
+      <div className=" w-1/2 h-screen flex flex-col justify-center px-24 -mt-20 leading-[6.2vw]">
         <h1 className=" text-[2vw] -mb-8 text-[#faddc5] font-['Saol_Display']">
           Log in
         </h1>
@@ -45,7 +47,7 @@ function Login() {
       </div>
       <div className=" w-1/2 h-screen uppercase py-20 pr-10 ">
         <div className="bg-[#fceadc] w-full h-[88vh] p-36 flex flex-col rounded-xl gap-32 ">
-          <form action={handleLogin}>
+          <form action="" onSubmit={(e) => handleLogin(e)}>
             <div className="flex flex-col">
               <div className="mb-8">
                 <h2 className="font-['GT_Walsheim'] font-black text-[3.2vw] text-[#3f4527]">
@@ -53,24 +55,26 @@ function Login() {
                 </h2>
               </div>
               <label
-                htmlFor=""
+                htmlFor="email"
                 className="font-['GT_Walsheim'] text-[#3f4527] font-black text-[1.4vw] mb-2"
               >
                 email
               </label>
               <input
                 type="text"
-                className="px-5 text-[1vw] border-[1px] bg-[#fceadc] border-[#3f4527] rounded-[5px] w-full h-[6vh]  mb-7 focus:outline-none focus:border-zinc-700 border-opacity-30 "
+                name="email"
+                className="px-5 text-[1vw] border-[1px] bg-[#fceadc] border-[#3f4527] rounded-[5px] w-full h-[6vh] mb-7 focus:outline-none focus:border-zinc-700 border-opacity-30 "
               />
               <label
-                htmlFor=""
+                htmlFor="password"
                 className="font-['GT_Walsheim'] text-[#3f4527] font-black text-[1.4vw] mb-2"
               >
                 password
               </label>
               <input
                 type="text"
-                className="px-5 text-[1vw] border-[1px] bg-[#fceadc] border-[#3f4527] rounded-[5px] w-full h-[6vh]  mb-10 focus:outline-none focus:border-zinc-700 border-opacity-30 "
+                name="password"
+                className="px-5 text-[1vw] border-[1px] bg-[#fceadc] border-[#3f4527] rounded-[5px] w-full h-[6vh] mb-10 focus:outline-none focus:border-zinc-700 border-opacity-30 "
               />
               <input
                 type="submit"
